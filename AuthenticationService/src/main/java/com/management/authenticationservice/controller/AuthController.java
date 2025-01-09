b00a505f88f154f07ed117b3a793c804feb9c567
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-10 01:26:07 +0300

    Add Exceptions

---
+++ AuthenticationService/src/main/java/com/management/authenticationservice/controller/AuthController.java
@@ -0,0 +1,67 @@
+package com.management.authenticationservice.controller;
+
+import com.management.authenticationservice.security.request.LoginRequest;
+import com.management.authenticationservice.security.request.SignupRequest;
+import com.management.authenticationservice.security.response.MessageResponse;
+import com.management.authenticationservice.security.response.UserInfoResponse;
+import com.management.authenticationservice.security.service.securityServiceInterface.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth/v1")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "Register a new user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Email already in use")
+    })
+    @PostMapping("/signup")
+    public ResponseEntity<MessageResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        return authService.registerUser(signUpRequest);
+    }
+
+    @Operation(summary = "Authenticate user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Login successful"),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials")
+    })
+    @PostMapping("/signin")
+    public ResponseEntity<Map<String, Object>> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.authenticateUser(loginRequest);
+    }
+
+    @Operation(summary = "Get logged-in user details")
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/user")
+    public ResponseEntity<UserInfoResponse> getUserDetails(Authentication authentication) {
+        return authService.getUserDetails(authentication);
+    }
+
+    @Operation(summary = "Sign out the user")
+    @PostMapping("/sign-out")
+    public ResponseEntity<MessageResponse> signOutUser() {
+        return authService.signoutUser();
+    }
+
+    @Operation(summary = "Check if the user is authenticated")
+    @GetMapping("/check")
+    public ResponseEntity<?> checkAuth(HttpServletRequest request) {
+        boolean isAuthenticated = authService.isAuthenticated(request);
+        return ResponseEntity.ok(Map.of("isAuthenticated", isAuthenticated));
+    }
+}
---
+++ AuthenticationService/src/main/java/com/management/authenticationservice/excepiton/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.management.authenticationservice.excepiton;
+
+public class ErrorMessages {
+    public static final String ROLE_NOT_FOUND = "Role not found: %s";
+    public static final String BAD_CREDENTIALS = "Invalid email or password";
+
+    private ErrorMessages() {
+    }
+}
