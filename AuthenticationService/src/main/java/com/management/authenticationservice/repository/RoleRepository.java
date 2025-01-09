3f1f9b72b0a0a7f3bc37123fa3d4d63dfb0ffe04
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-10 01:13:30 +0300

    Create Repositories

---
+++ AuthenticationService/src/main/java/com/management/authenticationservice/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.management.authenticationservice.repository;
+
+import com.management.authenticationservice.model.Role;
+import com.management.authenticationservice.model.enums.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRoleName(AppRole roleName);
+}
---
+++ AuthenticationService/src/main/java/com/management/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.management.authenticationservice.repository;
+
+import com.management.authenticationservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
