da578e4bd64435048d72b908f54334917a38cd93
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-16 00:37:25 +0300

    Add Exception

--- medicineservice/src/main/java/com/management/medicineservice/helper/ExcelHelper.java
+++ medicineservice/src/main/java/com/management/medicineservice/helper/ExcelHelper.java
@@ -7,6 +7,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,25 +18,33 @@ public class ExcelHelper {
 
     public List<Medicine> readMedicinesFromExcel(String filePath) {
         List<Medicine> medicines = new ArrayList<>();
+        Path path = Paths.get(filePath);
 
-        try (FileInputStream fileInputStream = new FileInputStream(filePath);
-             Workbook workbook = new XSSFWorkbook(fileInputStream)) {
-
-            Sheet sheet = workbook.getSheetAt(0);
-            int rowCount = sheet.getPhysicalNumberOfRows();
-
-            for (int i = 3; i < rowCount; i++) {
-                Row row = sheet.getRow(i);
-                if (row != null) {
-                    Medicine medicine = new Medicine();
-                    medicine.setName(getCellValue(row, 0));
-                    medicine.setBarcode(getCellValue(row, 1));
-                    medicine.setCompanyName(getCellValue(row, 4));
-                    medicine.setStatus(getCellValue(row, 6));
-                    medicines.add(medicine);
-                }
+        try {
+            long fileSize = Files.size(path);
+            if (fileSize > Integer.MAX_VALUE) {
+                throw new IOException("File size is too large");
             }
 
+            try (FileInputStream fileInputStream = new FileInputStream(filePath);
+                 Workbook workbook = new XSSFWorkbook(fileInputStream)) {
+
+                Sheet sheet = workbook.getSheetAt(0);
+                int rowCount = sheet.getPhysicalNumberOfRows();
+
+                for (int i = 3; i < rowCount; i++) {
+                    Row row = sheet.getRow(i);
+                    if (row != null) {
+                        Medicine medicine = new Medicine();
+                        medicine.setName(getCellValue(row, 0));
+                        medicine.setBarcode(getCellValue(row, 1));
+                        medicine.setCompanyName(getCellValue(row, 4));
+                        medicine.setStatus(getCellValue(row, 6));
+                        medicine.setPrice(Integer.parseInt(getCellValue(row, 5)));
+                        medicines.add(medicine);
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException("Error reading Excel file", e);
         }
@@ -51,5 +62,3 @@ public class ExcelHelper {
         };
     }
 }
-
-
