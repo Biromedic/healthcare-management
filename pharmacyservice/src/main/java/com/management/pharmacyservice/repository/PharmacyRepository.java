494d74fa0f8cc2a172d887506e1e0c49df56ec07
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-24 01:44:03 +0300

    Delete Pharmacy Service

--- pharmacyservice/src/main/java/com/management/pharmacyservice/repository/PharmacyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.management.pharmacyservice.repository;
-
-import com.management.pharmacyservice.model.Pharmacy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
-}
--- pharmacyservice/src/main/java/com/management/pharmacyservice/service/PharmacyService.java
+++
@@ -1,9 +0,0 @@
-package com.management.pharmacyservice.service;
-
-import com.management.pharmacyservice.dto.*;
-
-import java.util.List;
-
-public interface PharmacyService {
-    void processPrescription(PrescriptionProcessRequestDTO requestDTO);
-}
