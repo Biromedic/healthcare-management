4f07595b63827f52b7dd518cfe9a89e6d18055fd
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-25 02:18:52 +0300

    Add Exceptions

--- prescriptionservice/src/main/java/com/management/prescriptionservice/exception/ResourceNotFoundException.java
+++ prescriptionservice/src/main/java/com/management/prescriptionservice/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.management.prescriptionservice.exception.error;
+package com.management.prescriptionservice.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
