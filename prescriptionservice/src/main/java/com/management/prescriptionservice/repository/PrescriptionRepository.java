3ccb75807dfac4fde3d2e28ccbdbf89ec2e63f50
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-25 11:20:18 +0300

    Update Dependencies and Repository

--- prescriptionservice/src/main/java/com/management/prescriptionservice/repository/PrescriptionRepository.java
+++ prescriptionservice/src/main/java/com/management/prescriptionservice/repository/PrescriptionRepository.java
@@ -1,21 +1,16 @@
 package com.management.prescriptionservice.repository;
 
 import com.management.prescriptionservice.model.Prescription;
+import com.management.prescriptionservice.model.enums.PrescriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.lang.NonNull;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-    @NonNull
-    Page<Prescription> findAll(@NonNull Pageable pageable);
-
-    @Query("SELECT p FROM Prescription p WHERE p.isComplete = false AND p.missingDetails IS NOT NULL")
-    List<Prescription> findIncompletePrescriptions();
-
+    List<Prescription> findByStatusAndCreatedAtBetween(
+            PrescriptionStatus status,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 }
-
