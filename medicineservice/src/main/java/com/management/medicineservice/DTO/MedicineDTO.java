fc8a1dff76ad97b8c18c4b1addc3c992d84dbb4e
Birkan Cemil Abacı <69905454+Biromedic@users.noreply.github.com>
2025-01-28 19:38:16 +0300

    Add New Id Variable for Frontend Operations

--- medicineservice/src/main/java/com/management/medicineservice/DTO/MedicineDTO.java
+++ medicineservice/src/main/java/com/management/medicineservice/DTO/MedicineDTO.java
@@ -12,6 +12,7 @@ import java.io.Serializable;
 public class MedicineDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+    private String id;
     private String name;
     private Integer price;
 }
